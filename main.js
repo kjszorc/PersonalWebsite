38d6920a65dcd2a64206b3c892004b6d5d21b317
kjszorc <kjszorc@yahoo.com>
2019-07-09 20:55:20 -0500

    two way transitions, gradient update

---
+++ main.js
@@ -0,0 +1,130 @@
+$(document).ready(function() {
+  // Get the modal
+  var modal = document.getElementById("myModal");
+
+  // Get the button that opens the modal
+  var btn = document.querySelectorAll("li");
+// debugger;
+  // When the user clicks on the button, open the modal 
+  
+  btn.forEach(item => {
+    item.onclick = function() {
+      modal.style.display = "block";
+    }
+  });
+
+  var span = document.getElementById("close");
+
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function() {
+    modal.style.display = "none";
+  }
+
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function(event) {
+    // debugger;
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
+
+  // CONTACT FORM
+  $('#contact-form').submit(function(e) {
+    e.preventDefault();
+
+      $.ajax({
+          url: "https://formspree.io/kjszorc@yahoo.com",
+          method: "POST",
+          data: { message: $('form').serialize() },
+          dataType: "json"
+      }).done(function(response) {
+          window.alert('yay this may have worked!');
+          // $('#success').addClass('expand');
+          // $('#contact-form').find("input[type=text], input[type=email], textarea").val("");
+      });
+  });
+});
+
+
+
+
+// var space;
+
+// function floatySpace() {
+//   var colors = [
+//     "#FF3F8E", "#04C2C9", "#2E55C1"
+//   ];
+
+
+//   space = new CanvasSpace("canvas", "#252934" ).display();
+//   var form = new Form( space );
+
+//   // Elements
+//   var pts = [];
+//   var center = space.size.$divide(1.8);
+//   var angle = -(window.innerWidth * 0.5);
+//   var count = window.innerWidth * 0.05;
+//   if (count > 150) count = 150;
+//   var line = new Line(0, angle).to(space.size.x, 0);
+//   var mouse = center.clone();
+
+//   var r = Math.min(space.size.x, space.size.y) * 1;
+//   for (var i=0; i<count; i++) {
+//     var p = new Vector( Math.random()*r-Math.random()*r, Math.random()*r-Math.random()*r );
+//     p.moveBy( center ).rotate2D( i*Math.PI/count, center);
+//     p.brightness = 0.1
+//     pts.push( p );
+//   }
+
+//   // Canvas
+//   space.add({
+//     animate: function(time, fps, context) {
+
+//       for (var i=0; i<pts.length; i++) {
+//         // rotate the points slowly
+//         var pt = pts[i];
+
+//         pt.rotate2D( Const.one_degree / 20, center);
+//         form.stroke( false ).fill( colors[i % 3] ).point(pt, 1);
+
+//         // get line from pt to the mouse line
+//         var ln = new Line( pt ).to( line.getPerpendicularFromPoint(pt));
+
+//         // opacity of line derived from distance to the line
+//         var opacity = Math.min( 0.8, 1 - Math.abs( line.getDistanceFromPoint(pt)) / r);
+//         var distFromMouse = Math.abs(ln.getDistanceFromPoint(mouse))
+
+//         if (distFromMouse < 50) {
+//           if (pts[i].brightness < 0.3) pts[i].brightness += 0.015
+//         } else {
+//           if (pts[i].brightness > 0.1) pts[i].brightness -= 0.01
+//         }
+
+//         var color = "rgba(255,255,255," + pts[i].brightness +")"
+//         form.stroke(color).fill( true ).line(ln);
+//       }
+//     },
+
+//     onMouseAction: function(type, x, y, evt) {
+//       if (type=="move") {
+//         mouse.set(x,y);
+//       }
+//     },
+
+//     onTouchAction: function(type, x, y, evt) {
+//       this.onMouseAction(type, x, y);
+//     }
+//   });
+
+//   space.bindMouse();
+//   space.play();
+// }
+
+// floatySpace();
+
+// $(window).resize(function(){
+//   space.removeAll();
+//   $('canvas').remove();
+//   floatySpace();
+// });
+ 
